b318b8f1a78977d56650b1fc5ffc0990b31404d2
Matthew Clarke <m_d_clarke@hotmail.com>
2022-02-03 17:11:13 +0200

    created project template - added MUI and eslint-config-wesbos

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
+## Wordle Clone
+- This is a Wordle Clone using NextJS and MUI
+
 ## Getting Started
 
 First, run the development server:
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,26 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import styles from '../styles/Home.module.css';
+
+function BasicTextFields() {
+  return (
+    <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      <TextField id="filled-basic" label="Filled" variant="filled" />
+      <TextField id="standard-basic" label="Standard" variant="standard" />
+    </Box>
+  );
+}
 
 export default function Home() {
   return (
@@ -22,33 +42,7 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <BasicTextFields />
         </div>
       </main>
 
@@ -65,5 +59,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
+  );
 }
