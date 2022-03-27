d9bec763f8927b332e1c177b43e1faaa9877306c
Matthew Clarke <m_d_clarke@hotmail.com>
2022-03-27 13:49:01 +0200

    chore: issue with GitHub action - checking if authorization works for API

--- pages/api/random-nums-set.js
+++ pages/api/random-nums-set.js
@@ -31,6 +31,15 @@ export default async function handler(req, res) {
   if (req.method === 'POST') {
     try {
       const { authorization } = req.headers;
+      const { Authorization } = req.headers;
+      console.log(
+        'auth header === env var: ',
+        authorization === `Bearer ${process.env.ACTION_FETCH_RANDOM_NUM}`
+      );
+      console.log(
+        'Auth header === env var: ',
+        Authorization === `Bearer ${process.env.ACTION_FETCH_RANDOM_NUM}`
+      );
       if (authorization === `Bearer ${process.env.ACTION_FETCH_RANDOM_NUM}`) {
         await dbConnect();
 
